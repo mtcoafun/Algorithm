0306902a3b1741724b72a67ddabae93abbd47732
陈颀玮 <chenqiwei@chenqiweideMacBook-Pro.local>
2021-04-14 20:57:19 +0800

    2021/4/14 前缀树

---
+++ BinaryTree/前缀树.cpp
@@ -0,0 +1,61 @@
+#include <vector>
+#include <string>
+using namespace std;
+// Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
+// 请你实现 Trie 类：
+// Trie() 初始化前缀树对象。
+// void insert(String word) 向前缀树中插入字符串 word 。
+// boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+// boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
+// 示例：
+// 输入
+// ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+// [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+// 输出
+// [null, null, true, false, true, null, true]
+// 解释
+// Trie trie = new Trie();
+// trie.insert("apple");
+// trie.search("apple");   // 返回 True
+// trie.search("app");     // 返回 False
+// trie.startsWith("app"); // 返回 True
+// trie.insert("app");
+// trie.search("app");     // 返回 True
+class Trie {
+private:
+    vector<Trie*> children;
+    bool isWord;
+    Trie* lastNode(string str) {
+        Trie* node = this;
+        for (auto c : str) {
+            int tc = c - 'a';
+            if(!node->children[tc]) return nullptr;
+            else node = node->children[tc];
+        }
+        return node;
+    }
+public:
+    /** Initialize your data structure here. */
+    Trie() : children(26, nullptr), isWord(false) {}
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        Trie* node = this;
+        for(auto c : word) {
+            int tc = c - 'a';
+            if(!node->children[tc]) node->children[tc] = new Trie();
+            node = node->children[tc];
+        }
+        node->isWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        return lastNode(word) && lastNode(word)->isWord;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        return lastNode(prefix);
+    }
+};
